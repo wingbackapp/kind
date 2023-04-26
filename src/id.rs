5a5945ebb99b13ee31e329a57914cf0c10f04372
Denys Séguret <cano.petrole@gmail.com>
2023-04-26 03:32:55 +0200

    feat: <unrelated>
    <snip>

--- src/id.rs
+++ src/id.rs
@@ -2,6 +2,7 @@ use {
     super::*,
     sqlx::types::Uuid,
     std::{
+        cmp::Ordering,
         fmt,
         hash::{Hash, Hasher},
         marker::PhantomData,
@@ -52,6 +53,17 @@ impl<O: Identifiable> Clone for Id<O> {
     }
 }
 impl<O: Identifiable> Copy for Id<O> {}
+
+impl<O: Identifiable> PartialOrd for Id<O> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.uuid.partial_cmp(&other.uuid)
+    }
+}
+impl<O: Identifiable> Ord for Id<O> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.uuid.cmp(&other.uuid)
+    }
+}
 
 impl<O: Identifiable> Id<O> {
     /// Return the class of an id, usually mapped to a specific struct
@@ -113,5 +125,56 @@ impl<O: Identifiable> FromStr for Id<O> {
 impl<O: Identifiable> Hash for Id<O> {
     fn hash<H: Hasher>(&self, state: &mut H) {
         self.uuid().hash(state);
+    }
+}
+
+#[test]
+fn id_sorting() {
+    #[derive(Debug, Typid)]
+    #[typid(class = "Ex")]
+    pub struct E {}
+
+    let mut ids = Vec::<Id<E>>::new();
+    for _ in 0..10 {
+        ids.push(Id::random_v4());
+    }
+    let mut ids_by_string = ids.clone();
+
+    ids.sort();
+    ids_by_string.sort_by_key(|id| id.to_string());
+
+    // check that the Id order is the same than the
+    // one of their stringified representations
+    for (a, b) in ids.iter().zip(ids_by_string.iter()) {
+        assert_eq!(a, b);
+    }
+
+    // check that we can use a slice of ids as a sort key
+    // (this should be true as soon as id is Ord, but I
+    // prefer to be sure...)
+
+    let a = vec![ids[1]];
+    let b = vec![ids[1], ids[7]];
+    let c = vec![ids[3]];
+    let d = vec![ids[3], ids[2]];
+    let e = vec![ids[3], ids[2], ids[5]];
+    let f = vec![ids[3], ids[6]];
+    let g = vec![ids[3], ids[6], ids[8]];
+
+    let paths_1 = vec![
+        a.clone(),
+        b.clone(),
+        c.clone(),
+        d.clone(),
+        e.clone(),
+        f.clone(),
+        g.clone(),
+    ];
+    let mut paths_2 = vec![d, g, c, f, b, e, a];
+    paths_2.sort();
+    for (path_1, path_2) in paths_1.iter().zip(paths_2.iter()) {
+        for (a, b) in path_1.iter().zip(path_2.iter()) {
+            assert_eq!(a, b);
+        }
     }
 }
