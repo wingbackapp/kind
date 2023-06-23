999e2845d264e974cab949db776197398afd4f86
Oleksandr <o.muliar@wingback.com>
2023-06-23 13:55:52 +0300

    chore(openapi): document openapi objects
    * Full API object documentation

--- src/openapi.rs
+++ src/openapi.rs
@@ -1,7 +1,14 @@
+#[cfg(feature = "serde")]
+use serde_json::json;
 use utoipa::openapi::Schema;
 
 pub fn openapi_schema() -> (&'static str, Schema) {
-    let schema =
-        utoipa::openapi::ObjectBuilder::new().schema_type(utoipa::openapi::SchemaType::String);
+    let schema = utoipa::openapi::ObjectBuilder::new()
+        .schema_type(utoipa::openapi::SchemaType::String)
+        .description(Some(
+            "Unique identifier of an object. Consists of object class prefix and a UUID",
+        ));
+    #[cfg(feature = "serde")]
+    let schema = schema.example(Some(json!("Cust_c40bea18-c0c9-44b1-bd0c-43f5283e1670")));
     ("Id", schema.into())
 }
