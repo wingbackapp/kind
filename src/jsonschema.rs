b9847e75c376227e54f0615b0f4f9f5463cf4455
msd <o.muliar@wingback.com>
2024-02-08 13:21:44 +0200

    * Jonschema documentation * Jsonschema support for Ided * make openapi optional

--- src/jsonschema.rs
+++ src/jsonschema.rs
@@ -1,6 +1,11 @@
 use {
     super::*,
-    ::schemars::{schema::SchemaObject, JsonSchema},
+    ::schemars::{
+        gen::SchemaGenerator,
+        schema::{ObjectValidation, SchemaObject},
+        JsonSchema,
+    },
+    std::borrow::Cow,
 };
 
 impl<O: Identifiable> JsonSchema for Id<O> {
@@ -12,5 +17,38 @@ impl<O: Identifiable> JsonSchema for Id<O> {
         let mut schema: SchemaObject = <String>::json_schema(gen).into();
         schema.format = Some("string".to_owned());
         schema.into()
+    }
+}
+
+impl<O: Identifiable + JsonSchema> JsonSchema for Ided<O> {
+    fn schema_name() -> String {
+        format!("{}_ided", O::schema_name())
+    }
+
+    fn schema_id() -> Cow<'static, str> {
+        Cow::Owned(format!("Ided<{}>", O::schema_id()))
+    }
+
+    fn json_schema(gen: &mut SchemaGenerator) -> schemars::schema::Schema {
+        let subschema = O::json_schema(gen);
+        let mut properties = subschema
+            .into_object()
+            .object
+            .map(|obj| obj.properties)
+            .unwrap_or_default();
+        properties.insert("id".to_string(), Id::<O>::json_schema(gen));
+
+        schemars::schema::Schema::Object(SchemaObject {
+            instance_type: Some(schemars::schema::InstanceType::Object.into()),
+            metadata: Some(Box::new(schemars::schema::Metadata {
+                description: Some(format!("Identified version of {}", O::schema_name())),
+                ..Default::default()
+            })),
+            object: Some(Box::new(ObjectValidation {
+                properties,
+                ..Default::default()
+            })),
+            ..Default::default()
+        })
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -66,12 +66,13 @@ mod postgres;
 mod id_enum;
 #[cfg(feature = "jsonschema")]
 mod jsonschema;
+#[cfg(feature = "openapi")]
 mod openapi;
 #[cfg(feature = "serde")]
 mod serde_serialize;
 
 #[allow(unused_imports)]
-pub use {error::*, id::*, id_class::*, ided::*, identifiable::*, kind_proc::*, openapi::*};
+pub use {error::*, id::*, id_class::*, ided::*, identifiable::*, kind_proc::*};
 
 #[allow(unused_imports)]
 #[cfg(feature = "serde")]
@@ -81,17 +82,20 @@ pub use {crate::serde_serialize::*, id_enum::*};
 #[cfg(feature = "jsonschema")]
 pub use crate::jsonschema::*;
 
+#[allow(unused_imports)]
+#[cfg(feature = "openapi")]
+pub use crate::openapi::*;
+
 #[test]
 fn test_id_ided() {
-
     #[derive(Debug, Kind)]
-    #[kind(class="Cust")]
+    #[kind(class = "Cust")]
     pub struct Customer {
         pub name: String,
     }
 
     #[derive(Debug, Kind)]
-    #[kind(class="Cont")]
+    #[kind(class = "Cont")]
     pub struct Contract {
         // many fields
     }
@@ -104,23 +108,28 @@ fn test_id_ided() {
     );
 
     // You can parse the id from eg JSON, or just a string
-    let id: Id<Customer> = "Cust_371c35ec-34d9-4315-ab31-7ea8889a419a"
-        .parse().unwrap();
+    let id: Id<Customer> = "Cust_371c35ec-34d9-4315-ab31-7ea8889a419a".parse().unwrap();
 
     // The type is checked, so this id can't be misused as a contract id
-    assert!(
-        "Cust_371c35ec-34d9-4315-ab31-7ea8889a419a"
+    assert!("Cust_371c35ec-34d9-4315-ab31-7ea8889a419a"
         .parse::<Id<Contract>>()
-        .is_err()
-    );
+        .is_err());
 
     // The public id is parsed and checked in a case insensitive way
-    assert_eq!(id, "cust_371c35ec-34d9-4315-ab31-7ea8889a419a".parse().unwrap());
-    assert_eq!(id, "CUST_371C35EC-34D9-4315-AB31-7EA8889A419A".parse().unwrap());
+    assert_eq!(
+        id,
+        "cust_371c35ec-34d9-4315-ab31-7ea8889a419a".parse().unwrap()
+    );
+    assert_eq!(
+        id,
+        "CUST_371C35EC-34D9-4315-AB31-7EA8889A419A".parse().unwrap()
+    );
 
     // You can build an identified object from just
     // Here's a new customer:
-    let new_customer = Customer { name: "John".to_string() };
+    let new_customer = Customer {
+        name: "John".to_string(),
+    };
     // Give it an id, by wrapping it in an Ided
     let customer = Ided::new(id, new_customer);
 
@@ -134,10 +143,9 @@ fn test_id_ided() {
 #[cfg(feature = "serde")]
 #[test]
 fn test_serde() {
-
     // deserialize a customer
     #[derive(Debug, Kind, serde::Serialize, serde::Deserialize)]
-    #[kind(class="Cust")]
+    #[kind(class = "Cust")]
     pub struct Customer {
         pub name: String,
     }
@@ -161,5 +169,4 @@ fn test_serde() {
         "name": "John"
     }"#;
     assert!(serde_json::from_str::<Ided<Customer>>(&json).is_err());
-
 }
